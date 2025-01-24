94f6a5ffe43be1be5d46b0a81584722bcb6e4955
WillianM19 <willianm1928@gmail.com>
2025-01-24 14:05:51 -0300

    test: teste ci/cd

--- send.py
+++ send.py
@@ -7,7 +7,7 @@ channel = connection.channel()
 channel.queue_declare(queue='notify')
 
 
-content = "dinizrobert2002gmail.com,flechotonshabbos@gmail.com;conteudo do relatório"
+content = "lechotonshabbos@gmail.com;conteudo do relatório"
 
 channel.basic_publish(exchange='', routing_key='notify', body=content)
 print(" [x] Sent 'data'")
